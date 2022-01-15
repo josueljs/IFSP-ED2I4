3c9d26a635c4538d066644af85ec4aa81e2edb6d
Josuel Santos <josuel.santos@aluno.ifsp.edu.br>
2022-01-14 22:41:38 -0300

    Add files via upload

---
+++ Atividade22-01-05/Equipamento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atividade22_01_05
+{
+    public class Equipamento
+    {
+        private int id;
+        private bool avaria;
+
+        public int Id { get => id; set => id = value; }
+        public bool Avaria { get => avaria; set => avaria = value; }
+
+        public Equipamento()
+        {
+            this.id = 0;
+            this.avaria = false;
+        }
+
+        public Equipamento(int id)
+        {
+            this.id = id;
+            this.avaria = false;
+        }
+    }
+}
