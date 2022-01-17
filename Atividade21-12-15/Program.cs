c3130b01cc34cc6353891071099c05797c5bf63b
Josuel Santos <josuel.santos@aluno.ifsp.edu.br>
2022-01-17 14:31:13 -0300

    Add files via upload

---
+++ Atividade21-12-15/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade21_12_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
